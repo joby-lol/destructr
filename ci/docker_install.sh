d28ad59c62629094d5ece47dc6e98900f37a7e9c
Joby Elliott <joby@byjoby.com>
2018-08-21 19:44:52 -0600

    switching to alpine PHP for speed

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -6,13 +6,13 @@
 set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
-apt-get update -yqq
-apt-get install git -yqq
-
-# Install phpunit, the tool that we will use for testing
-curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
-chmod +x /usr/local/bin/phpunit
+apk update
+apk add git
 
 # Install mysql driver
 # Here you can install any other extension that you need
 docker-php-ext-install pdo_mysql pdo_sqlite
+
+# Install composer
+curl -sS https://getcomposer.org/installer | php
+php composer.phar install

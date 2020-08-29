94adce7d045dffa8b0e1d79fd875fdb4524a6fc8
Joby Elliott <code@byjoby.com>
2020-08-29 11:33:30 -0600

    travis fix?

--- tests/Drivers/MariaDB/MariaDBDriverTest.php
+++ tests/Drivers/MariaDB/MariaDBDriverTest.php
@@ -10,7 +10,7 @@ class MariaDBDriverTest extends AbstractSQLDriverTest
 {
     const DRIVER_CLASS = MariaDBDriver::class;
     const DRIVER_DSN = 'mysql:host=127.0.0.1;port=3307;dbname=destructrtest';
-    const DRIVER_USERNAME = 'destructrtest';
-    const DRIVER_PASSWORD = 'destructrtest';
+    const DRIVER_USERNAME = 'root';
+    const DRIVER_PASSWORD = 'verysecret';
     const DRIVER_OPTIONS = null;
 }
